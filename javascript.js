75959414bdba1257c6fcdc8bdba09646db7d687f
MuhammadAndi8 <152310574+MuhammadAndi8@users.noreply.github.com>
2024-12-08 22:37:03 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,58 @@
+const image = document.querySelector(".responsive");
+function updateImage() {
+  if (window.innerWidth <= 768) {
+    image.src = "asset/mobileip12.jpg";
+  } else {
+    image.src = "asset/dekstoppc.jpg";
+  }
+}
+window.addEventListener("load", updateImage);
+window.addEventListener("resize", updateImage);
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+  tombolMenu.click(function () {
+    menu.toggle();
+  });
+  menu.click(function () {
+    menu.toggle();
+  });
+}
+
+$(document).ready(function () {
+  var width = $(window).width();
+  if (width < 990) {
+    klikMenu();
+  }
+});
+
+//check lebar
+$(window).resize(function () {
+  var width = $(window).width();
+  if (width > 989) {
+    menu.css("display", "block");
+    //display:block
+  } else {
+    menu.css("display", "none");
+  }
+  klikMenu();
+});
+
+//efek scroll
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 0) {
+      $("nav").addClass("putih");
+      $("nav img.hitam").show();
+      $("nav img.putih").hide();
+    } else {
+      $("nav").removeClass("putih");
+      $("nav img.hitam").hide();
+      $("nav img.putih").show();
+    }
+  });
+});
